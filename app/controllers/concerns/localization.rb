d6505f4458060f74d37abcbb93d7afa9c2c96fef
antarr <antarr.byrd@gmail.com>
2025-06-09 22:14:30 -0500

    frozen_string_literal

--- app/controllers/concerns/localization.rb
+++ app/controllers/concerns/localization.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Localization
   extend ActiveSupport::Concern
 
--- app/jobs/sms_verification_job.rb
+++ app/jobs/sms_verification_job.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class SmsVerificationJob < ApplicationJob
   queue_as :default
 
